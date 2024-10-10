bf7a7eecf7dfa2dc190c6f14b0f9a3123c10d0d0
Atikur Rahman Rajib <114246760+atikurajib@users.noreply.github.com>
2024-10-10 18:26:44 +0600

    Add files via upload

---
+++ 6. display_variables_2.java
@@ -0,0 +1,20 @@
+// 
+
+public class Main {
+	
+    public static void main(String[] args) {
+    	
+        String firstName = "Ahmed";
+        String lastName = "Robin";
+        String fullName = firstName + " " + lastName;
+        
+        System.out.println(fullName);
+        
+        int x = 23;
+        int y = 27;
+        // int x = 23, y = 27;
+        
+        System.out.println(x + y);
+    
+    }   
+}
