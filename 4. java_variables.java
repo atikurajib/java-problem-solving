2bd12dabce55bd26c1b2fdce3829c0c64d31cf2f
Atikur Rahman Rajib <114246760+atikurajib@users.noreply.github.com>
2024-10-10 18:20:15 +0600

    Add files via upload

---
+++ 4. java_variables.java
@@ -0,0 +1,28 @@
+// java variables
+
+public class Main {
+	
+    public static void main(String[] args) {
+    	
+        String name = "Ahmed";
+        System.out.println(name);
+        
+        int number1 = 11;
+        System.out.println(number1);
+        
+        float number2 = 4.25f;
+        System.out.println(number2);
+        
+        double number3 = 3.33;
+        System.out.println(number3);
+        
+        char ch = 'A';
+        System.out.println(ch);
+        
+        boolean decision = true;
+        System.out.println(decision);
+    
+    }
+    
+}
+
---
+++ 8. calculate_the_area_of_rectangle.java
@@ -0,0 +1,16 @@
+// Calculate the Area of a Rectangle
+
+public class Main {
+	public static void main(String[] args) {
+    	int length = 10;
+        int width = 15;
+        int area;
+        
+        area = length * width;
+        
+        System.out.println("Length: " + length);
+        System.out.println("Width: " + width);
+        System.out.println("Area of Rectangle: " + area);
+    }
+}
+
