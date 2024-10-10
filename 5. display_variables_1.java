b792e1e1f56a4362022a6e2a2ce589717c46316d
Atikur Rahman Rajib <114246760+atikurajib@users.noreply.github.com>
2024-10-10 18:26:22 +0600

    Add files via upload

---
+++ 5. display_variables_1.java
@@ -0,0 +1,14 @@
+// display variables
+
+public class Main {
+	
+    public static void main(String[] args) {
+    	
+        String name = "Ahmed";
+        System.out.println("Hi " + name);
+        
+        int number = 11;
+        System.out.println(name +" " +number);
+        
+    }
+}
