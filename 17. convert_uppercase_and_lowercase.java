e9d647ed6c74a64afc66a16121fe1342441b7269
Atikur Rahman Rajib <114246760+atikurajib@users.noreply.github.com>
2024-10-15 18:26:54 +0600

    Add files via upload

---
+++ 17. convert_uppercase_and_lowercase.java
@@ -0,0 +1,9 @@
+// convert uppercase and lowercase in java
+
+public class Main {
+  public static void main(String[] args) {
+    String txt = "Atikur Rahman Rajib";
+    System.out.println(txt.toUpperCase());
+    System.out.println(txt.toLowerCase());
+  }
+}
---
+++ 18. find_a_string_position.java
@@ -0,0 +1,9 @@
+// find a string position
+
+public class Main {
+  public static void main(String[] args) {
+    String txt = "My name is Rajib";
+    System.out.println(txt.indexOf("Rajib")); // R or Rajib , the output will be 11
+  }
+}
+
